9b8ea8814e8cb7b0c2a785718feb48012a8b75a5
Sravanth Chebrolu <sravanth.chebro@gmail.com>
2020-08-18 20:07:36 +0530

    basic css

--- src/components/List.js
+++ src/components/List.js
@@ -1,35 +1,41 @@
-import React , {useContext,useEffect, useState} from 'react';
-import {GlobalContext} from '../context/GlobalState';
+import React, { useContext, useEffect, useState } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import './List.css';
 import Meeting from './MeetingItem';
 const List = (props) => {
-    const {getMeetings, meetings, curr, today} = useContext(GlobalContext);
-    useEffect(()=>{
+    const { getMeetings, meetings, curr, today } = useContext(GlobalContext);
+    useEffect(() => {
         getMeetings('');
         //eslint-disable-next-line react-hooks/exhaustive-deps
-    } ,[]);
+    }, []);
     const handleAdd = () => {
         props.history.push('/addmeeting');
-    }
+    };
     const prevClick = () => {
-        getMeetings('prev')
-    }
+        getMeetings('prev');
+    };
     const nextClick = () => {
         getMeetings('next');
         // disabled={today > curr}
-    }
+    };
     return (
-        <div>
-        <button onClick={() => prevClick()} >Prev</button> {curr}/08/2020
-        <button onClick={() => nextClick()}>Next</button>
+        <div className="main">
+            <div className="navigator">
+                <button className="btn-prev" onClick={() => prevClick()}>Prev
+                <i className="fa fa-angle-left" aria-hidden="true"></i>
+                </button>
+                {curr} Aug 2020
+                <button className="btn-next" onClick={() => nextClick()}>Next</button>
+            </div>
             {meetings.map(meeting => (
-                <Meeting meeting = {meeting} key={meeting.start_time + meeting.end_time} />
+                <Meeting meeting={meeting} key={meeting.start_time + meeting.end_time} />
             ))}
 
-        <button disabled={today > curr} onClick={() => handleAdd()}>
-            Add Meeting
+            <button className="btn-add" disabled={today > curr} onClick={() => handleAdd()}>
+                Add Meeting
         </button>
         </div>
     );
-}
- 
+};
+
 export default List;
--- src/components/MeetingItem.js
+++ src/components/MeetingItem.js
@@ -1,25 +1,27 @@
-import React , {useState, useEffect} from 'react'
-const Meeting = ({meeting}) => {
+import React, { useState, useEffect } from 'react';
+import './MeetingItem.css';
+const Meeting = ({ meeting }) => {
     const [pers, setPers] = useState('');
     useEffect(() => {
-        var users = ''; 
-        for(var i of meeting.participants){
-            users+= ` ${i} `
+        var users = '';
+        for (var i of meeting.participants) {
+            users += ` ${i} `;
         }
-        setPers(users)
-    }, [])
+        setPers(users);
+    }, []);
     // participants = 
     return (
         <div>
-            <li className="list-item">
-            {/* start_time: 12:30,
+            <div className="meet-item">
+                {/* start_time: 12:30,
             //     end_time: 13:00,
             //     description: "development team of XYZ project: brainstorming session",
             //     participants: ["Sahil Arora", "Neha"] */}
-                Meeting from {meeting.start_time} to {meeting.end_time} on {meeting.description} with {pers}
-            </li>
+                <div className="meet-time">{meeting.start_time} - {meeting.end_time}</div>
+                <div className="meet-desc">{meeting.description} with {pers}</div>
+            </div>
         </div>
     );
-}
- 
+};
+
 export default Meeting;
